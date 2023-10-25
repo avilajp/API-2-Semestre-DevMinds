7de386bd4bf12b6784944cd872e52b1d79017c78
Andre Wakugawa <andre.wakugawa@outlook.com>
2023-10-25 12:05:06 -0300

    fix AlunoDao | feat ProfessorDao

--- AppTG/DBConnector/src/main/java/devminds/tgcontrol/dao/AlunoDao.java
+++ AppTG/DBConnector/src/main/java/devminds/tgcontrol/dao/AlunoDao.java
@@ -7,30 +7,30 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class AlunoDao {
-    public void registerAluno(String aluno_email_fatec,String aluno_email_pessoal, String nome_aluno) {
+    public void registerAluno(String aluno_email_pessoal,String aluno_email_fatec, String nome_aluno) {
         try (Connection con = SqlConnection.getConnection()) {
-            String sql_update = "UPDATE sgtg.aluno SET aluno_email_fatec = ?,aluno_email_pessoal = ?, nome = ? where aluno_email_fatec = ?";
+            String sql_update = "UPDATE sgtg.aluno SET aluno_email_pessoal = ?, aluno_email_fatec = ?, nome = ? where aluno_email_pessoal = ?";
             PreparedStatement pst;
             pst = con.prepareStatement(sql_update);
-            pst.setString(1, aluno_email_fatec);
-            pst.setString(2,aluno_email_pessoal);
+            pst.setString(1, aluno_email_pessoal);
+            pst.setString(2,aluno_email_fatec);
             pst.setString(3, nome_aluno);
-            pst.setString(4,aluno_email_fatec);
+            pst.setString(4,aluno_email_pessoal);
 
             int updatedRowCount = pst.executeUpdate();
 
             if (updatedRowCount == 0) {
-                String sql_insert = "INSERT INTO sgtg.aluno (aluno_email_fatec, aluno_email_pessoal, nome) VALUES (?, ?, ?)";
+                String sql_insert = "INSERT INTO sgtg.aluno (aluno_email_pessoal, aluno_email_fatec, nome) VALUES (?, ?, ?)";
                 PreparedStatement pst2;
                 pst2 = con.prepareStatement(sql_insert);
-                pst2.setString(1, aluno_email_fatec);
-                pst2.setString(2, aluno_email_pessoal);
+                pst2.setString(1, aluno_email_pessoal);
+                pst2.setString(2, aluno_email_fatec);
                 pst2.setObject(3,nome_aluno);
                 pst2.executeUpdate();
             }
         } catch (SQLException e) {
             e.printStackTrace();
-            throw new RuntimeException("Erro ao gerar nova atividade!!");
+            throw new RuntimeException("Erro ao registrar aluno !!");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
