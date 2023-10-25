8df69f0dac52dcc499ce838b8349611e1f5dcd2a
João Pedro Ávila <avilassp@gmail.com>
2023-10-25 20:40:53 -0300

    Atualização Aval

--- AppTG/AppTGScreen/src/main/java/devminds/tgcontrol/apptg/ControllerTelaAvaliacao.java
+++ AppTG/AppTGScreen/src/main/java/devminds/tgcontrol/apptg/ControllerTelaAvaliacao.java
@@ -6,11 +6,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class ControllerTelaAvaliacao {
+    @FXML private TextField nomeAluno;
+
     @FXML
     private void stageToMainScree(ActionEvent event) throws IOException {
         Parent tableViewParent = FXMLLoader.load(getClass().getResource("TelaInicial.fxml"));
